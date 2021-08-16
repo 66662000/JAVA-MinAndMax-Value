6c2791daf1ec5a1e8b0278386f48662dfac26cd3
JEAN-FELIX Jean Ricardo <82624815+66662000@users.noreply.github.com>
2021-08-16 16:48:42 -0400

    Add files via upload

---
+++ SomeMaths/src/MathsJava.java
@@ -0,0 +1,13 @@
+
+public class MathsJava {
+
+	public static void main(String[] args) {
+		double x=4, y=-45;
+		double z=Math.max(x, y);
+		System.out.println("Le plus grand nombre est: "+z);
+		double m= Math.min(y, z);
+		System.out.println("Le plus petit nombre est: "+m);
+
+	}
+
+}
